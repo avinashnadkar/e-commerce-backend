68d80d3bb4b445d6aca61e8c2905f394b908e3bc
avinashnadkar <avinashnadkar3@gmail.com>
2022-03-24 19:05:50 +0530

    created api to add orders

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,29 @@
+//Middleware to check user is authorized 
+
+require('dotenv').config();
+const JWT = require('jsonwebtoken');
+
+module.exports = async (req,res,next) => {
+
+    const token = req.header('x-auth-token')
+
+    if(!token){
+        return res.status(400).json({
+            "errors":[{
+                "msg" : "No token found"
+            }]
+        })
+    }
+
+    try{
+       let user =  JWT.verify(token,process.env.KEY);
+        next();
+    }catch(err){
+        return res.status(400).json({
+            "errors":[{
+                "msg" : "Token invalid"
+            }]
+        })
+    }
+
+}
--- models/productModel.js
+++ models/productModel.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
         type : String,
         required : true
     },
-    Price : {
+    price : {
         type : Number,
         required : true
     },
